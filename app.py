e5dd703d8dd735f91eb730fb9b651e9499980bd7
milonstratlytics <96039135+milonstratlytics@users.noreply.github.com>
2023-04-17 13:47:26 +0530

    code

--- app.py
+++ app.py
@@ -1,24 +1,59 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask
+#!/usr/bin/env python
+# coding: utf-8
 
-# Flask constructor takes the name oq
-# current module (__name__) as argument.
-app = Flask(__name__)
+# In[10]:
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-	return 'Hello World'
 
-# main driver function
-if __name__ == '__main__':
 
-	# run() method of Flask class runs the application
-	# on the local development server.
-	app.run()
+import numpy as np
+import pandas as pd
+import streamlit as st 
+
+def calc(x,y,req):
+    x=float(x)
+    y=float(y)
+    if (req=='+'):
+        prediction=x+y
+    elif(req=='-'):
+        prediction=x-y
+    elif(req=='*'):
+        prediction=x*y
+    elif(req=='/'):
+        prediction=x/y
+    
+    print(prediction)
+    return prediction
+
+
+
+def main():
+    st.title("Calculator")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Calculator ML App </h2>
+    <p style="color:white;text-align:center;">(use +,-,*,/ for any operation) </p>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    x = st.text_input("X","Type Here")
+    y = st.text_input("Y","Type Here")
+    req = st.text_input("Provide symbol for any operation","Type Here")
+    result=""
+    if st.button("Predict"):
+        result=calc(x,y,req)
+    st.success('The output is {}'.format(result))
+    if st.button("About"):
+        st.text("Lets LEarn")
+        st.text("Built with Streamlit")
+
+if __name__=='__main__':
+    main()
+    
+    
+    
+
+
+
+
 
 
