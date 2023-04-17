1da2e292a19070306fedaf007b0b527b15645d43
milonstratlytics <96039135+milonstratlytics@users.noreply.github.com>
2023-04-17 13:55:06 +0530

    new1

--- app.py
+++ app.py
@@ -1,58 +1,35 @@
-#!/usr/bin/env python
-# coding: utf-8
+from flask import Flask, render_template, request
 
-# In[10]:
+app = Flask(__name__)
+# app.config.from_object(__name__)
 
+@app.route('/')
+def welcome():
+    return render_template('form.html')
 
+@app.route('/', methods=['POST'])
+def result():
+    var_1 = request.form.get("var_1", type=int, default=0)
+    var_2 = request.form.get("var_2", type=int, default=0)
+    operation = request.form.get("operation")
+    if(operation == 'Addition'):
+        result = var_1 + var_2
+    elif(operation == 'Subtraction'):
+        result = var_1 - var_2
+    elif(operation == 'Multiplication'):
+        result = var_1 * var_2
+    elif(operation == 'Division'):
+    	if(var_1==0 and var_2==0):
+    		result = 0
+    	else:
+        	result = var_1 / var_2
+    else:
+        result = 0
+    entry = result
+    return render_template('form.html', entry=entry)
 
-import numpy as np
-import pandas as pd
-import streamlit as st 
-
-def calc(x,y,req):
-    x=float(x)
-    y=float(y)
-    if (req=='+'):
-        prediction=x+y
-    elif(req=='-'):
-        prediction=x-y
-    elif(req=='*'):
-        prediction=x*y
-    elif(req=='/'):
-        prediction=x/y
-    
-    print(prediction)
-    return prediction
-
-
-
-def main():
-    st.title("Calculator")
-    html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Calculator ML App </h2>
-    <p style="color:white;text-align:center;">(use +,-,*,/ for any operation) </p>
-    </div>
-    """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    x = st.text_input("X","Type Here")
-    y = st.text_input("Y","Type Here")
-    req = st.text_input("Provide symbol for any operation","Type Here")
-    result=""
-    if st.button("Predict"):
-        result=calc(x,y,req)
-    st.success('The output is {}'.format(result))
-    if st.button("About"):
-        st.text("Lets LEarn")
-        st.text("Built with Streamlit")
-
-if __name__=='__main__':
-    main()
-    
-    
-    
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
